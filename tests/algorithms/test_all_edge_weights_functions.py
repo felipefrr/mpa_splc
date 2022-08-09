63f3ca777c1070f120ab4a14e5fa928f64cc33f1
Felipe Ferreira <felipe.moura10@gmail.com>
2022-08-09 02:56:58 -0300

    Wrote the unit tests for SPLC function optimized and node weights functions

--- tests/algorithms/test_all_edge_weights_functions.py
+++ tests/algorithms/test_all_edge_weights_functions.py
@@ -1,41 +1,47 @@
 import networkx as nx
+import pytest
 
 from src.algorithms.graphs import get_syncs, get_sources, compare_graphs
-from src.algorithms.edge_weights import calculate_spc, calculate_splc_fast, calculate_splc
-from utils.loading import get_all_input_graphml_files_path, get_all_output_csv_files_path, load_graphml_file, load_csv_file
+from src.algorithms.edge_weights import calculate_spc, calculate_splc_fast, calculate_splc, calculate_splc_optimized
+from utils.loading import get_all_input_graphml_files_path, get_all_output_csv_files_path, load_graphml_file, \
+    load_csv_file
 
 
-def basic_digraph():
-    edges = [['A', 'C'], ['C', 'E'], ['C', 'H'], ['E', 'G'], ['G', 'H'], ['H', 'K'],
-             ['B', 'C'], ['B', 'D'], ['B', 'J'], ['D', 'F'], ['D', 'I'], ['F', 'H'],
-             ['F', 'I'], ['I', 'L'], ['I', 'M'], ['J', 'M'], ['M', 'N']]
-    G = nx.DiGraph()
-    G.add_edges_from(edges)
-    return G, edges
+class DigraphExamples:
+    def __init__(self):
+        self.edges = [['A', 'C'], ['C', 'E'], ['C', 'H'], ['E', 'G'], ['G', 'H'], ['H', 'K'],
+                      ['B', 'C'], ['B', 'D'], ['B', 'J'], ['D', 'F'], ['D', 'I'], ['F', 'H'],
+                      ['F', 'I'], ['I', 'L'], ['I', 'M'], ['J', 'M'], ['M', 'N']]
+        self.G = nx.DiGraph()
+        self.G.add_edges_from(self.edges)
+        self.sources = get_sources(self.G)
+        self.syncs = get_syncs(self.G)
 
 
-def test_calculate_spc_correctness():
-    G, edges = basic_digraph()
-    calculate_spc(G, get_sources(G), get_syncs(G))
+@pytest.fixture()
+def di_G():
+    return DigraphExamples()
+
+
+def test_calculate_spc_correctness(di_G):
+    calculate_spc(di_G.G, di_G.sources, di_G.syncs)
     spc_expected = [2, 2, 2, 2, 2, 5, 2, 5, 1, 3, 2, 1, 2, 2, 2, 1, 3]
-    for i in range(len(edges)):
-        assert G[edges[i][0]][edges[i][1]]["SPC"] == spc_expected[i]
+    for i in range(len(di_G.edges)):
+        assert di_G.G[di_G.edges[i][0]][di_G.edges[i][1]]["SPC"] == spc_expected[i]
 
 
-def test_calculate_splc_fast_correctness():
-    G, edges = basic_digraph()
-    calculate_splc_fast(G, get_sources(G), get_syncs(G))
+def test_calculate_splc_fast_correctness(di_G):
+    calculate_splc_fast(di_G.G, di_G.sources, di_G.syncs)
     splc_expected = [2, 3, 3, 4, 5, 12, 2, 5, 1, 6, 4, 3, 6, 6, 6, 2, 9]
-    for i in range(len(edges)):
-        assert G[edges[i][0]][edges[i][1]]["SPLC"] == splc_expected[i]
+    for i in range(len(di_G.edges)):
+        assert di_G.G[di_G.edges[i][0]][di_G.edges[i][1]]["SPLC"] == splc_expected[i]
 
 
-def test_calculate_splc_correctness():
-    G, edges = basic_digraph()
-    calculate_splc(G, get_syncs(G))
+def test_calculate_splc_correctness(di_G):
+    calculate_splc(di_G.G, di_G.syncs)
     splc_expected = [2, 3, 3, 4, 5, 12, 2, 5, 1, 6, 4, 3, 6, 6, 6, 2, 9]
-    for i in range(len(edges)):
-        assert G[edges[i][0]][edges[i][1]]["SPLC"] == splc_expected[i]
+    for i in range(len(di_G.edges)):
+        assert di_G.G[di_G.edges[i][0]][di_G.edges[i][1]]["SPLC"] == splc_expected[i]
 
 
 def test_splc_correctness_with_inputs():
@@ -49,3 +55,10 @@ def test_splc_correctness_with_inputs():
         calculate_splc_fast(graph1, get_sources(graph1), get_syncs(graph1))
         graph2 = output_graphs[index]
         assert compare_graphs(graph1, graph2)
+
+
+def test_splc_optimal_correctness(di_G):
+    calculate_splc_optimized(di_G.G, di_G.syncs)
+    splc_expected = [2, 3, 3, 4, 5, 12, 2, 5, 1, 6, 4, 3, 6, 6, 6, 2, 9]
+    for i in range(len(di_G.edges)):
+        assert di_G.G[di_G.edges[i][0]][di_G.edges[i][1]]["SPLC"] == splc_expected[i]
---
+++ tests/algorithms/test_all_node_weights_functions.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import pytest
+
+from src.algorithms.graphs import get_syncs
+from src.algorithms.node_weights import calculate_n_all_minus, calculate_n_plus
+
+
+class DigraphExamples:
+    def __init__(self):
+        self.edges = [['A', 'C'], ['C', 'E'], ['C', 'H'], ['E', 'G'], ['G', 'H'], ['H', 'K'],
+                      ['B', 'C'], ['B', 'D'], ['B', 'J'], ['D', 'F'], ['D', 'I'], ['F', 'H'],
+                      ['F', 'I'], ['I', 'L'], ['I', 'M'], ['J', 'M'], ['M', 'N']]
+        self.G = nx.DiGraph()
+        self.G.add_edges_from(self.edges)
+        self.nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
+        self.syncs = get_syncs(self.G)
+        self.topologic_order = list(nx.topological_sort(self.G))
+
+
+@pytest.fixture()
+def di_G():
+    return DigraphExamples()
+
+
+def test_calculate_n_minus_all_correctness(di_G):
+    for node in di_G.topologic_order:
+        calculate_n_all_minus(di_G.G, node)
+
+    n_minus_all_expected = [1, 1, 3, 2, 2, 4, 3, 5, 6, 12, 7, 9, 13, 10]
+    for index, node in enumerate(di_G.topologic_order):
+        assert di_G.G.nodes[node]["Nall-"] == n_minus_all_expected[index]
+
+
+def test_calculate_n_plus_correctness(di_G):
+    reversed_top_order = list(reversed(di_G.topologic_order))
+    for node in reversed_top_order:
+        calculate_n_plus(di_G.G, node, di_G.syncs)
+
+    n_plus_expected = [1, 1, 1, 1, 1, 2, 1, 3, 1, 1, 5, 2, 8, 2]
+    for index, node in enumerate(reversed_top_order):
+        assert di_G.G.nodes[node]["N+"] == n_plus_expected[index]
