4fcfaebe4ef8c9c59899f3b9c0e096c922693c12
Felipe Ferreira <felipe.moura10@gmail.com>
2022-08-09 02:58:06 -0300

    Implemented the benchmark for optimized SPLC function & updated pylintrc

--- benchmarks/benchmark_edge_weights_functions.py
+++ benchmarks/benchmark_edge_weights_functions.py
@@ -1,41 +1,47 @@
 import networkx as nx
 import pytest
 
-from src.algorithms.edge_weights import calculate_spc, calculate_splc_fast, calculate_splc
+from src.algorithms.edge_weights import calculate_spc, calculate_splc_fast, calculate_splc, calculate_splc_optimized
 from src.algorithms.graphs import get_syncs, get_sources
 
 
-def basic_digraph():
-    edges = [['A', 'C'], ['C', 'E'], ['C', 'H'], ['E', 'G'], ['G', 'H'], ['H', 'K'],
-             ['B', 'C'], ['B', 'D'], ['B', 'J'], ['D', 'F'], ['D', 'I'], ['F', 'H'],
-             ['F', 'I'], ['I', 'L'], ['I', 'M'], ['J', 'M'], ['M', 'N']]
-    G = nx.DiGraph()
-    G.add_edges_from(edges)
-    return G, edges
+class DigraphExamples:
+    def __init__(self):
+        self.edges = [['A', 'C'], ['C', 'E'], ['C', 'H'], ['E', 'G'], ['G', 'H'], ['H', 'K'],
+                      ['B', 'C'], ['B', 'D'], ['B', 'J'], ['D', 'F'], ['D', 'I'], ['F', 'H'],
+                      ['F', 'I'], ['I', 'L'], ['I', 'M'], ['J', 'M'], ['M', 'N']]
+        self.G = nx.DiGraph()
+        self.G.add_edges_from(self.edges)
+        self.sources = get_sources(self.G)
+        self.syncs = get_syncs(self.G)
+
+
+@pytest.fixture()
+def di_G():
+    return DigraphExamples()
 
 
 @pytest.mark.benchmark(group="SPC")
-def test_calc_spc(benchmark):
-    G, _ = basic_digraph()
-
+def test_calc_spc(benchmark, di_G):
     @benchmark
     def spc():
-        calculate_spc(G, get_sources(G), get_syncs(G))
+        calculate_spc(di_G.G, di_G.sources, di_G.syncs)
 
 
 @pytest.mark.benchmark(group="SPLC")
-def test_calc_splc(benchmark):
-    G, _ = basic_digraph()
-
+def test_calc_splc(benchmark, di_G):
     @benchmark
     def splc():
-        calculate_splc(G, get_syncs(G))
-
+        calculate_splc(di_G.G, di_G.syncs)
 
 @pytest.mark.benchmark(group="SPLC")
-def test_calc_splc_fast(benchmark):
-    G, _ = basic_digraph()
-
+def test_calc_splc_fast(benchmark, di_G):
     @benchmark
     def splc_fast():
-        calculate_splc_fast(G, get_sources(G), get_syncs(G))
+        calculate_splc_fast(di_G.G, di_G.sources, di_G.syncs)
+
+@pytest.mark.benchmark(group="SPLC")
+def test_calc_splc_optimized(benchmark, di_G):
+    @benchmark
+    def splc_fast():
+        calculate_splc_optimized(di_G.G, di_G.syncs)
