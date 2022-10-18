4a6dd4fa3b2f16e2bbef602e7714e14a1907ed9a
Felipe Ferreira <felipe.moura10@gmail.com>
2022-10-18 19:19:35 -0300

    Fixing script issues

--- mpa_starting_pack.py
+++ mpa_starting_pack.py
@@ -1,11 +1,13 @@
 # Importando todas as funções que precisamos:
-from src.algorithms.edge_weights import calculate_splc_optimized
-from src.algorithms.dag import get_syncs, add_artificial_source_sync, remove_anomalies, remove_cycles
-from src.algorithms.paths import main_path
-
 import os
 import pandas as pd
-import neetwork as nx
+import networkx as nx
+
+from src.algorithms.edge_weights import calculate_splc_optimized
+from src.algorithms.dag import get_syncs, add_artificial_source_sync, simplify, remove_anomalies, remove_cycles
+from src.algorithms.paths import main_path
+
+
 
 # ------ Como carregar um arquivo? ------
 """
@@ -24,25 +26,32 @@ G = nx.from_pandas_edgelist(
     data,
     source='Source',
     target='Target',
+    edge_attr='pln_date',
     create_using=nx.DiGraph()
 )
 
 # Adicionamos a informação da pln_date, salvamos essa informação em seu respectivo nó:
 nx.set_node_attributes(G, pd.Series(data.pln_date.values, index=data.Source).to_dict(), "pln_date")
 
-# ------ Como remover o ciclo? ------
-# Para remover ciclo basta chamar a função remove_cycles da remove_cycles em algorithms/dag.py
-edges_removes = remove_cycles(G)
-# Essa função remove os ciclos inplace.
+# Vamos remover as arestas anômalas
+edges_removed = remove_anomalies(G)
 
+# ------ Como remover o ciclo? ------
+# Para melhorar a performance da remoção de ciclos, criei uma função que simplifica a rede
+H = simplify(G)
+
+# Para remover ciclo basta chamar a função remove_cycles da remove_cycles em algorithms/dag.py
+edges_to_remove = remove_cycles(H)
+
+# Agora vamos efetivamente remover todas as arestas que causam ciclos
+G.remove_edges_from(edges_to_remove)# Essa função remove os ciclos inplace.
 
 
 # ------ Como printar o SPLC? ------
 # Antes de printar o SPLC, precisamos calculá-lo primeiro.
 
-# Adcionamos um source e um sync artificial para simplificar a busca.
+# Adicionamos um source e um sync artificial para simplificar a busca.
 add_artificial_source_sync(G)
-
 
 syncs = get_syncs(G)
 
@@ -54,7 +63,8 @@ calculate_splc_optimized(G, syncs)
 # Para printar o SPLC, precisamores iterar sobre todas as arestas do grafo e mostrar seus valores:
 edges = list(map(list, G.edges()))
 for edge in edges:
-    print(G[edge[0]][edge[1]]["SPLC"])
+    print("SPLC({} -> {}) = {}".format(edge[0], edge[1], G[edge[0]][edge[1]]["SPLC"]))
+# É muito aresta para imprimir, seria melhor guardar num arquivo.
 
 # ------ Como calcular o Main Path? ------
 path = main_path(G)
